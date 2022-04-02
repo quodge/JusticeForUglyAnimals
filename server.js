22c894804d3f40caa2847df20ea745ebb0129f25
quodge <scottjallen20@gmail.com>
2022-04-02 17:46:11 +0100

    added routing to server.js

--- server.js
+++ server.js
@@ -6,7 +6,24 @@ const PORT = process.env.PORT || 8080;
 var port = PORT;
 //send index.html file to the user for the home page
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    //create routes for admin section
+    var adminRouter = express.Router();
+    //admin main page. the dashboard (http://locahost:PORT/admin)
+    adminRouter.get('/', function(req, res){
+        res.sendFile(__dirname + '/index.html');
+    });
+    //users page(http://localhost:Port/admin/users)
+    adminRouter.get('/users', function(req, res){
+        res.send('I show all the users!');
+    });
+    //posts page (http://localhost:PORT/admin/posts)
+    adminRouter.get('/posts', function(req, res){
+        res.send('I show all the posts!'); 
+    });
+
+    //Apply the routes to the app
+    app.use(/'admin', adminRouter);
+    
 });
 //Start server
 app.listen(PORT);
