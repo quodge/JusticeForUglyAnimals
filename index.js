afdc209183eb19f49ca404577ce0f0b6ec459ad7
quodge <scottjallen20@gmail.com>
2022-04-02 16:53:22 +0100

    edited index.js for heroku

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var http = require("http");
 
 http.createServer(function (request, response){
 
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, {'Content-Type': 'text/plain',
+                            'Access-Control-Allow-Origin' : '*'
+    });
+//var readStream = fs.createReadStream(__dirname + '/index.html');
+//readStream.pipe(response);
+    response.end('Hello World\n');
+
+    }).listen(process.env.PORT || 8080);
 
     response.end('Hello World\n');
-}).listen(8080);
+//}).listen(8080);
 
-console.log('Server running at http://127.0.0.1:8080/');
+console.log('Server running on port ${ PORT }');
