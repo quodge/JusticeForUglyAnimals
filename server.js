fee3d95c8c6205070ed58dbc52f1e2818d940a14
quodge <scottjallen20@gmail.com>
2022-04-04 11:45:01 +0100

    Uncomment basic data transfer

--- server.js
+++ server.js
@@ -18,18 +18,18 @@ var port = PORT;
 
 
 
-//app.route('/login')
- //   .get(function(req, res) {    
-//        var output = 'getting the login! ';
-//        var input1 = req.query['input1'];
-//        var input2 = req.query['input2'];
-//        console.log('The params:'+ req.query.input1 + " " + req.query.input2);
- //       
- //   })
- //   // process the form (POST http://localhost:PORT/login)
- //   .post(function(req, res) { console.log('processing');
- //   res.send('processing the login form!');
-//  });
+app.route('/login')
+    .get(function(req, res) {    
+       var output = 'getting the login! ';
+       var input1 = req.query['input1'];
+       var input2 = req.query['input2'];
+       console.log('The params:'+ req.query.input1 + " " + req.query.input2);
+       
+   })
+   // process the form (POST http://localhost:PORT/login)
+   .post(function(req, res) { console.log('processing');
+   res.send('processing the login form!');
+ });
 
 //send index.html file to the user for the home page
 app.get('/', function(req, res){
