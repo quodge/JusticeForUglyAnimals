8c7edfc064c4e22728dc36e44a0d88740a2c5d9b
quodge <scottjallen20@gmail.com>
2022-04-11 14:23:11 +0100

    try to add json web tokens  to comments page

--- server.js
+++ server.js
@@ -245,6 +245,23 @@ const comment = mongoose.model('Comment', commentsSchema);
 //Comments page
 // var commentsRouter = express.Router();
 app.get('/comments' ,(req, res) => {
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).end()
+    }
+
+    var payload
+
+    try{
+        payload = jwt.verify(token, jwtKey)
+    }catch (e){
+        if(e instanceof jwt.JsonWebTokenError){
+            return res.status(401).end()
+        }
+        return res.status(400).end()
+    }
+    
     comment.find({}, function(err, comments){
         res.render('pages/Comments', {
             commentsList: comments
