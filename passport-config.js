319421a245fd29135094b4384700ae6c423a17e9
quodge <scottjallen20@gmail.com>
2022-04-11 11:54:47 +0100

    removed old bcrypt code

--- passport-config.js
+++ passport-config.js
@@ -1,42 +1,42 @@
-const LocalStrategy = require('passport-local').Strategy
-const bcrypt = require('bcrypt')
+// const LocalStrategy = require('passport-local').Strategy
+// const bcrypt = require('bcrypt')
 
-function initialize(passport, getUserByUsername, getUserById){
-    const authenticateUser = async (username, password, done) => {
-        const user = getUserByUsername(username)
-        if (user == null){
-            return done(null, false, {message: 'No user with that username'})
-        }
+// function initialize(passport, getUserByUsername, getUserById){
+//     const authenticateUser = async (username, password, done) => {
+//         const user = getUserByUsername(username)
+//         if (user == null){
+//             return done(null, false, {message: 'No user with that username'})
+//         }
 
-        try {
-            if (await bcrypt.compare(password, user.password)){
-                return done(null, user)
-            } else{
-                return done(null, false, {message: 'Password incorrect'})
-            }
-        }catch (e){
-            console.log('Problem in authenticateUser Try/catch')
-            return done(e)
-        }
+//         try {
+//             if (await bcrypt.compare(password, user.password)){
+//                 return done(null, user)
+//             } else{
+//                 return done(null, false, {message: 'Password incorrect'})
+//             }
+//         }catch (e){
+//             console.log('Problem in authenticateUser Try/catch')
+//             return done(e)
+//         }
 
 
 
        
-        //Code for password if unhashed //////////////////////
-        // if(password == user.password){
-        //     return done(null, user)
-        // }else{
-        //     return done(null, false, {message: 'Password incorrect'})
-        // }
-        ////////////////////////////////////////////////////
-    }
-    passport.use(new LocalStrategy({ usernameField: 'username'}, 
-    authenticateUser))
-    passport.serializeUser((user, done) => done(null, user.id))
-    passport.deserializeUser((id, done) => { 
-        return done(null, getUserById(id))
-    })
+//         //Code for password if unhashed //////////////////////
+//         // if(password == user.password){
+//         //     return done(null, user)
+//         // }else{
+//         //     return done(null, false, {message: 'Password incorrect'})
+//         // }
+//         ////////////////////////////////////////////////////
+//     }
+//     passport.use(new LocalStrategy({ usernameField: 'username'}, 
+//     authenticateUser))
+//     passport.serializeUser((user, done) => done(null, user.id))
+//     passport.deserializeUser((id, done) => { 
+//         return done(null, getUserById(id))
+//     })
 
-}
+// }
 
-module.exports = initialize
+// module.exports = initialize
