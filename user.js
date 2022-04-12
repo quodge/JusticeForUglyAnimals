45b8142689fb75f9a1935500ec3c685a6b158d7a
quodge <scottjallen20@gmail.com>
2022-04-12 05:18:36 +0100

    Removed old code

--- user.js
+++ user.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
+// const mongoose = require('mongoose');
+// const passportLocalMongoose = require('passport-local-mongoose');
 
-const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+// const uri = process.env.MONGODB_URI;
+// mongoose.connect(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
 
-//user login schema
-const Schema = mongoose.Schema;
-const User = new Schema({
-    username: String,
-    password: String
-});
+// //user login schema
+// const Schema = mongoose.Schema;
+// const User = new Schema({
+//     username: String,
+//     password: String
+// });
 
-User.plugin(passportLocalMongoose);
+// User.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('userData', User, 'userData');
+// module.exports = mongoose.model('userData', User, 'userData');
