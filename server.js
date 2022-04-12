ece0c0983e3884d15226ad4e590f0d1827d1cb6f
quodge <scottjallen20@gmail.com>
2022-04-12 12:01:58 +0100

    try push again

--- server.js
+++ server.js
@@ -487,7 +487,7 @@ function checkUserIsAdmin(req, res){
         }
         
         if (payload.username != "AdminLFTU"){
-            res.redirect('/unauthorised')
+            res.redirect('/unauthorised') 
         }
     }
 
